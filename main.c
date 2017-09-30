6cc669cba2994edb10f639246f585254f22abeff
Ayaka Neko <neko@ayaka.moe>
2017-09-30 20:45:21 +0800

    Init Commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//Init Commit
+
+/***************************************************************
+ * Project: moeBot-server
+ * Author: Ayaka Neko nekomeowww@github
+ * Repo: https://github.com/moeFramework/moeBot-server
+ * License: GNU General Public License v3.0
+ * 
+ * Copyright (C) 2017  Ayaka Neko
+ ***************************************************************/
+ 
+ void main(int argc, char **argv)
+ {
+	 //Todo - Follow up with Todonium
+	 return;
+ }
